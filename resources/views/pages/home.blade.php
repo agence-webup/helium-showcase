aeb0f541e6741db3568b1bca2aeb996bae57d814
val <valentin@agence-webup.com>
2023-05-29 08:54:51 +0200

    chore: install tailwindcss

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Helium Showcase</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-slate-200">
+    <h1 class="text-4xl text-center text-slate-800">Helium Showcase</h1>
+</body>
+
+</html>
