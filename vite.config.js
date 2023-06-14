42b46a9cac657c23cab8dd27af17ce6e460f8231
val <valentin@agence-webup.com>
2023-06-14 16:39:17 +0200

    refact: move js/css files to helium-ui repo
    Because these must be published from the package.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['packages/helium-ui/resources/css/app.css', '/packages/helium-ui/resources/js/app.js'],
             refresh: true,
         }),
     ],
