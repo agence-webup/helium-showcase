711e0d2d4ecc260702fd68bf09736b3c32f099ec
Robin Parisi <parisi.robin@gmail.com>
2023-09-27 18:05:57 +0200

    refact: move css and js to core

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -24,102 +24,103 @@
 @endsection
 @section('content')
     <x-helium-ui::filters>
-        <x-helium-ui::filters.item label="tab 1" />
-        <x-helium-ui::filters.item label="tab 2" />
-        <x-helium-ui::filters.item label="tab 3" />
-        <x-helium-ui::filters.item label="tab 4" />
+        <x-helium-ui::filters.item label="All" filter="all" />
+        <x-helium-ui::filters.item label="Stats" filter="ui1" />
+        <x-helium-ui::filters.item label="Misc" filter="ui2" />
+        <x-helium-ui::filters.item label="Forms" filter="ui3" />
+        <x-helium-ui::filters.item label="Datatable" filter="ui4" />
     </x-helium-ui::filters>
-    <div class="grid grid-cols-4 gap-5">
-        <x-helium-ui::stats title="Titre 1" value="Valeur 1" />
-        <x-helium-ui::stats title="Titre 2" value="Valeur 2" />
-        <x-helium-ui::stats title="Titre 3" value="Valeur 3" />
-        <x-helium-ui::stats title="Titre 4" value="Valeur 4" />
+    <div data-helium-filter-target="ui1">
+        <div class="grid grid-cols-4 gap-5">
+            <x-helium-ui::stats title="Titre 1" value="Valeur 1" />
+            <x-helium-ui::stats title="Titre 2" value="Valeur 2" />
+            <x-helium-ui::stats title="Titre 3" value="Valeur 3" />
+            <x-helium-ui::stats title="Titre 4" value="Valeur 4" />
+        </div>
     </div>
-    <div class="grid grid-cols-3 gap-5">
+    <div data-helium-filter-target="ui2">
+        <div class="grid grid-cols-3 gap-5">
+            <x-helium-ui::box>
+                <x-slot:title>Tags</x-slot:title>
+                <x-helium-ui::tag label="gray" modifier="gray" />
+                <x-helium-ui::tag label="red" modifier="red" />
+                <x-helium-ui::tag label="yellow" modifier="yellow" />
+                <x-helium-ui::tag label="green" modifier="green" />
+                <x-helium-ui::tag label="blue" modifier="blue" />
+                <x-helium-ui::tag label="indigo" modifier="indigo" />
+                <x-helium-ui::tag label="purple" modifier="purple" />
+                <x-helium-ui::tag label="pink" modifier="pink" />
+                <x-helium-ui::tag label="orange" modifier="orange" />
+            </x-helium-ui::box>
+            <x-helium-ui::box>
+                <x-slot:title>
+                    Dots
+                </x-slot:title>
+                <x-helium-ui::dot modifier="gray" />
+                <x-helium-ui::dot modifier="red" />
+                <x-helium-ui::dot modifier="yellow" />
+                <x-helium-ui::dot modifier="green" />
+                <x-helium-ui::dot modifier="blue" />
+                <x-helium-ui::dot modifier="indigo" />
+                <x-helium-ui::dot modifier="purple" />
+                <x-helium-ui::dot modifier="pink" />
+                <x-helium-ui::dot modifier="orange" />
+            </x-helium-ui::box>
+            <x-helium-ui::box>
+                <x-slot:title>Notifications</x-slot:title>
+                <x-helium-ui::button label="Success" modifier="ghost" onclick="Helium.notif.success('Wubalubadubdub')" />
+                <x-helium-ui::button label="Error" modifier="ghost" onclick="Helium.notif.error('Wubalubadubdub')" />
+                <x-helium-ui::button label="Warning" modifier="ghost" onclick="Helium.notif.open({type:'warning', message: 'Wubalubadubdub' })" />
+                <x-helium-ui::button label="Info" modifier="ghost" onclick="Helium.notif.open({type:'info', message: 'Wubalubadubdub' })" />
+            </x-helium-ui::box>
+        </div>
+    </div>
+    <div data-helium-filter-target="ui3">
         <x-helium-ui::box>
-            <x-slot:title>Tags</x-slot:title>
-            <x-helium-ui::tag label="gray" modifier="gray" />
-            <x-helium-ui::tag label="red" modifier="red" />
-            <x-helium-ui::tag label="yellow" modifier="yellow" />
-            <x-helium-ui::tag label="green" modifier="green" />
-            <x-helium-ui::tag label="blue" modifier="blue" />
-            <x-helium-ui::tag label="indigo" modifier="indigo" />
-            <x-helium-ui::tag label="purple" modifier="purple" />
-            <x-helium-ui::tag label="pink" modifier="pink" />
-            <x-helium-ui::tag label="orange" modifier="orange" />
-        </x-helium-ui::box>
-        <x-helium-ui::box>
-            <x-slot:title>
-                Dots
-            </x-slot:title>
-            <x-helium-ui::dot modifier="gray" />
-            <x-helium-ui::dot modifier="red" />
-            <x-helium-ui::dot modifier="yellow" />
-            <x-helium-ui::dot modifier="green" />
-            <x-helium-ui::dot modifier="blue" />
-            <x-helium-ui::dot modifier="indigo" />
-            <x-helium-ui::dot modifier="purple" />
-            <x-helium-ui::dot modifier="pink" />
-            <x-helium-ui::dot modifier="orange" />
-        </x-helium-ui::box>
-        <x-helium-ui::box>
-            <x-slot:title>Notifications</x-slot:title>
-            <x-helium-ui::button label="Success" modifier="ghost" onclick="Helium.notif.success('Wubalubadubdub')" />
-            <x-helium-ui::button label="Error" modifier="ghost" onclick="Helium.notif.error('Wubalubadubdub')" />
-            <x-helium-ui::button label="Warning" modifier="ghost"
-                onclick="Helium.notif.open({type:'warning', message: 'Wubalubadubdub' })" />
-            <x-helium-ui::button label="Info" modifier="ghost"
-                onclick="Helium.notif.open({type:'info', message: 'Wubalubadubdub' })" />
+            <x-slot:title>Forms</x-slot:title>
+            <form method="POST" action="/test">
+                @csrf
+                <div class="space-y-4">
+                    <x-helium-ui::form.input name="firstname" label="Firstname" type="text" class="max-w-md" />
+                    <x-helium-ui::form.input name="email" label="Email" type="email" info="Additional information on this field." class="max-w-md" />
+                    <x-helium-ui::form.textarea name="description" label="Description" info="Additional information on this field." class="max-w-lg" rows="5" />
+                </div>
+
+                <div class="mb-3 mt-3 grid grid-cols-3 gap-5">
+                    <x-helium-ui::form.input name="input_1" label="Password" type="password" />
+                    <x-helium-ui::form.input name="input_2" label="Number" type="number" />
+                    <x-helium-ui::form.input name="input_3" label="Date" type="date" />
+                </div>
+
+                <div class="flex max-w-md items-end gap-4">
+                    <x-helium-ui::form.input name="input_4" label="Start" type="date" class="h-10" />
+                    <x-helium-ui::form.input name="input_5" label="End" type="date" class="h-10" />
+                    <x-helium-ui::button label="Delete" icon="tabler-trash" modifier="danger" class="h-10" />
+                </div>
+
+                <x-helium-ui::divider />
+
+                <div class="mb-5 mt-3">
+                    <x-helium-ui::form.checkbox name="checkbox" label="Checkboxes">
+                        <x-helium-ui::form.checkbox.item name="checkbox[]" value="checkbox_1" label="label 1" />
+                        <x-helium-ui::form.checkbox.item name="checkbox[]" value="checkbox_2" label="label 2" />
+                        <x-helium-ui::form.checkbox.item name="checkbox[]" value="checkbox_3" label="label 3" />
+                        <x-helium-ui::form.checkbox.item name="checkbox[]" value="checkbox_4" label="label 4" />
+                    </x-helium-ui::form.checkbox>
+                </div>
+
+                <div>
+                    <x-helium-ui::button label="Test errors" type="submit" />
+                </div>
+            </form>
         </x-helium-ui::box>
     </div>
 
-
-
-    <x-helium-ui::box>
-        <x-slot:title>Forms</x-slot:title>
-        <form method="POST" action="/test">
-            @csrf
-            <div class="space-y-4">
-                <x-helium-ui::form.input name="firstname" label="Firstname" type="text" class="max-w-md" />
-                <x-helium-ui::form.input name="email" label="Email" type="email"
-                    info="Additional information on this field." class="max-w-md" />
-                <x-helium-ui::form.textarea name="description" label="Description"
-                    info="Additional information on this field." class="max-w-lg" rows="5" />
-            </div>
-
-            <div class="mb-3 mt-3 grid grid-cols-3 gap-5">
-                <x-helium-ui::form.input name="input_1" label="Password" type="password" />
-                <x-helium-ui::form.input name="input_2" label="Number" type="number" />
-                <x-helium-ui::form.input name="input_3" label="Date" type="date" />
-            </div>
-
-            <div class="flex max-w-md items-end gap-4">
-                <x-helium-ui::form.input name="input_4" label="Start" type="date" class="h-10" />
-                <x-helium-ui::form.input name="input_5" label="End" type="date" class="h-10" />
-                <x-helium-ui::button label="Delete" icon="tabler-trash" modifier="danger" class="h-10" />
-            </div>
-
-            <x-helium-ui::divider />
-
-            <div class="mb-5 mt-3">
-                <x-helium-ui::form.checkbox name="checkbox" label="Checkboxes">
-                    <x-helium-ui::form.checkbox.item name="checkbox[]" value="checkbox_1" label="label 1" />
-                    <x-helium-ui::form.checkbox.item name="checkbox[]" value="checkbox_2" label="label 2" />
-                    <x-helium-ui::form.checkbox.item name="checkbox[]" value="checkbox_3" label="label 3" />
-                    <x-helium-ui::form.checkbox.item name="checkbox[]" value="checkbox_4" label="label 4" />
-                </x-helium-ui::form.checkbox>
-            </div>
-
-            <div>
-                <x-helium-ui::button label="Test errors" type="submit" />
-            </div>
-        </form>
-
-    </x-helium-ui::box>
-
-    <x-helium-ui::box>
-        <x-slot:title>Datatable</x-slot:title>
-        <livewire:category-datatable sharedKey="category-datatable" paginationSize=10 />
-        {{-- <livewire:category-sidebar sharedKey="category-datatable" /> --}}
-    </x-helium-ui::box>
+    <div data-helium-filter-target="ui4">
+        <x-helium-ui::box>
+            <x-slot:title>Datatable</x-slot:title>
+            <livewire:category-datatable sharedKey="category-datatable" paginationSize=10 />
+            {{-- <livewire:category-sidebar sharedKey="category-datatable" /> --}}
+        </x-helium-ui::box>
+    </div>
 @endsection
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['/resources/css/admin/app.css', '/resources/js/admin/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['/packages/helium-core/resources/css/app.css', '/packages/helium-core/resources/js/helium.js'],
+      refresh: true,
+    }),
+  ],
 });
